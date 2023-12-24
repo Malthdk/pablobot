924cdd2a52c35d8a4b8cea1e9b2661756964ddfc
Malthdk <malte.holledig@zeronorth.com>
2023-12-24 08:53:13 +0100

    first commit

---
+++ src/bot.ts
@@ -0,0 +1,58 @@
+import Telegram from "node-telegram-bot-api";
+import { OpenAI } from "openai";
+import puppeteer from "puppeteer";
+import { credentials } from "../credentials";
+
+const bot = new Telegram(credentials.telegramToken, { polling: true });
+const openai = new OpenAI({ apiKey: credentials.openAIKey });
+
+export const scraper = async (url: string, selector: string) => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(url);
+
+  await page.waitForSelector(selector);
+  const data = await page.$eval(selector, (el) => el.textContent);
+
+  await browser.close();
+
+  return data;
+};
+
+export const handler = async () => {
+  const weatherForecast = await scraper(
+    "https://www.dmi.dk/lokation/show/DK/2618425/K%C3%B8benhavn/",
+    ".weather-forecast"
+  );
+
+  if (!weatherForecast) return "No weather forecast found";
+
+  const completion = await openai.chat.completions.create({
+    messages: [
+      {
+        role: "system",
+        content:
+          "You will act as a prompt generator. I will describe weather conditions for you, and you will create a prompt that could be used for image generation. The image must also have a narrative element and tell a story. The styles should be either cubism, surrealism, abstract, digital art or impressionism. You must pick a style randomly.",
+      },
+      { role: "user", content: weatherForecast },
+    ],
+    model: "gpt-4",
+  });
+
+  const prompt = completion.choices[0].message.content;
+  return prompt;
+  if (!prompt) return "No prompt generated";
+
+  //   const image = await openai.images.generate({
+  //     model: "dall-e-3",
+  //     prompt,
+  //     n: 1,
+  //     size: "1024x1024",
+  //   });
+
+  //   if (!image.data[0].url) return "No image generated";
+
+  //   if (image) bot.sendPhoto(credentials.chatId, image.data[0].url);
+};
+
+bot.on("message", handler);
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,19 @@
+import cron from "@elysiajs/cron";
 import { Elysia } from "elysia";
+import { handler } from "./bot";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+  .use(
+    cron({
+      name: "daily-art",
+      pattern: "0 6 * * *",
+      run() {
+        handler;
+      },
+    })
+  )
+  .get("/", handler)
+  .listen(8080);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
