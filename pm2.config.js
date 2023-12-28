2e8215b360570ee3ef4e1f7a0e88d153f94e6262
Malthdk <malte.holledig@zeronorth.com>
2023-12-28 09:12:04 +0100

    small adjustments

--- pm2.config.js
+++ pm2.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   name: "pablobot", // Name of your application
   script: "src/index.ts", // Entry point of your application
-  interpreter: "~/.bun/bin/bun", // Path to the Bun interpreter
+  interpreter: "/home/malth/.bun/bin/bun", // Path to the Bun interpreter
 };
