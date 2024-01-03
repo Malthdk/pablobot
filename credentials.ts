72184f07d79b4acc539d88da8dcf5c93e4fdff90
Malthdk <malte.holledig@zeronorth.com>
2024-01-03 21:50:54 +0100

    removing EVERYTHING

---
+++ credentials.ts
@@ -0,0 +1,5 @@
+export const credentials = {
+  telegramToken: "6358500124:AAHb6foSqSVUXw-jqAntX7KQhbd0nKJUohI",
+  chatId: "6964904227",
+  openAIKey: "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",
+};
