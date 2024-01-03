227bbaf4a3b25be95999ea6e080eea5ce7e556d2
Malthdk <malte.holledig@zeronorth.com>
2024-01-03 22:03:50 +0100

    feat: added cron

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import fastify from "fastify";
 import { handler } from "./bot";
 
+const cron = require("node-cron");
+
 const server = fastify({ logger: true });
 
 server.get("/", handler);
+cron.schedule("0 6 * * *", handler);
 
 server.listen({ port: 8080 }, (err, address) => {
   if (err) {
